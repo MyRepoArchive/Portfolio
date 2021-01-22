0f9d10abeea1b56a93d072ebbc1ea43157854384
João Victor <joaovictor-san-co@hotmail.com>
2021-01-21 21:15:43 -0300

    update

--- scripts/showCredits.js
+++ scripts/showCredits.js
@@ -2,15 +2,16 @@ const creditosButton = document.querySelector('#container-creditos-button button
 const creditosDiv = document.querySelector('.container-creditos');
 const creditos = document.querySelector('#creditos');
 
-creditosDiv.style.top = 'calc(100vh - 1rem)'
+creditosDiv.style.top = 'calc(100vh - 1rem)';
+creditosButton.style.top = 'calc(100vh - 2rem)';
 
 creditosButton.addEventListener('click', () => {
   if (creditosDiv.style.top === 'calc(100vh - 1rem)') {
     creditosDiv.style.top = '2rem'
-    creditosButton.style.bottom = 'calc(100vh - 2rem - 1.2rem)'
+    creditosButton.style.top = '1rem';
   } else {
     creditosDiv.style.top = 'calc(100vh - 1rem)';
-    creditosButton.style.bottom = '0';
+    creditosButton.style.top = 'calc(100vh - 2rem)';
   };
 });
 
