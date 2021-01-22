2b1259010025c826d87814dc664bbd05dbbc7adc
João Victor <joaovictor-san-co@hotmail.com>
2021-01-22 17:34:14 -0300

    update

--- scripts/showCredits.js
+++ scripts/showCredits.js
@@ -2,16 +2,16 @@ const creditosButton = document.querySelector('#container-creditos-button button
 const creditosDiv = document.querySelector('.container-creditos');
 const creditos = document.querySelector('#creditos');
 
-creditosDiv.style.top = 'calc(100vh - 1rem)';
-creditosButton.style.top = 'calc(100vh - 2rem)';
+creditosDiv.style.top = '100vh';
+creditosButton.style.top = 'calc(100vh - 1.6rem)';
 
 creditosButton.addEventListener('click', () => {
-  if (creditosDiv.style.top === 'calc(100vh - 1rem)') {
-    creditosDiv.style.top = `calc(100% - 10rem - ${creditos.querySelector('p').innerText.length / 30}rem)`
-    creditosButton.style.top = `calc(100% - 11rem - ${creditos.querySelector('p').innerText.length / 30}rem)`;
+  if (creditosDiv.style.top === '100vh') {
+    creditosDiv.style.top = `calc(100% - 7rem - ${creditos.querySelector('p').innerText.length / 30}rem)`
+    creditosButton.style.top = `calc(100% - 8rem - ${creditos.querySelector('p').innerText.length / 30}rem)`;
   } else {
-    creditosDiv.style.top = 'calc(100vh - 1rem)';
-    creditosButton.style.top = 'calc(100vh - 2rem)';
+    creditosDiv.style.top = '100vh';
+    creditosButton.style.top = 'calc(100vh - 1.6rem)';
   };
 });
 
